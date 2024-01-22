70351967b34c48e824109e1729bfa0a0c4a947fb
Shai Mann-Robison <mann.robison@gmail.com>
2024-01-22 16:38:48 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import re, sys
+
+from PyQt5.QtWidgets import QApplication
+from gui import WidgetGallery
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    gallery = WidgetGallery()
+    gallery.show()
+    sys.exit(app.exec())
